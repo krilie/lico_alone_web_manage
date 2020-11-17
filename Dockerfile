98de31760feefab6b3fb2fd85e21c2d98922c052
Administrator <1197829331@qq.com>
2020-11-17 17:30:31 +0800

    ckeditor5 init

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,9 @@ MAINTAINER livo
 
 ADD ./ /lico_web_manage
 WORKDIR /lico_web_manage
-RUN cd /lico_web_manage/ckeditor5 && npm install && npm run build \
+RUN cd /lico_web_manage/ckeditor5-custom-build && npm install && npm run build \
     && cd /lico_web_manage \
-    && npm link ./ckeditor5 \
+    && npm link ./ckeditor5-custom-build \
     && npm install --production && npm run-script build
 
 FROM nginx:stable-alpine
--- src/pages/management/manage-page/article-page/ArticleEditPage.js
+++ src/pages/management/manage-page/article-page/ArticleEditPage.js
@@ -6,7 +6,8 @@ import TextArea from "antd/lib/input/TextArea";
 import AutoCompleteImageFile from "../../../../components/imageFile/AutoCompleteImageFile";
 import ArrowLeftOutlined from "@ant-design/icons/lib/icons/ArrowLeftOutlined";
 import SelectFileModal from "../../../../components/imageFile/SelectFileModal";
-
+import ClassicEditor from "ckeditor5-custom-build"
+import { CKEditor } from '@ckeditor/ckeditor5-react';
 
 class ArticleEditPage extends Component {
 
@@ -120,7 +121,27 @@ class ArticleEditPage extends Component {
         const contentView = <Row>
             <Col span={24}>
                 <Form.Item name="content">
-                    <TextArea autoSize={{maxRows: 60, minRows: 6}} defaultValue="" placeholder="请输入"/>
+                    <CKEditor
+                        config={{
+
+                        }}
+                        editor={ ClassicEditor }
+                        data="<p>Hello from CKEditor 5!</p>"
+                        onReady={ editor => {
+                            // You can store the "editor" and use when it is needed.
+                            console.log( 'Editor is ready to use!', editor );
+                        } }
+                        onChange={ ( event, editor ) => {
+                            const data = editor.getData();
+                            console.log( { event, editor, data } );
+                        } }
+                        onBlur={ ( event, editor ) => {
+                            console.log( 'Blur.', editor );
+                        } }
+                        onFocus={ ( event, editor ) => {
+                            console.log( 'Focus.', editor );
+                        } }
+                    />
                 </Form.Item>
             </Col>
         </Row>
